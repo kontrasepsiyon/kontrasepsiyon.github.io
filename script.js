e76f2a6999e2596ec568bad6960c0302f84d40d2
Alp <alpguvenir@Alps-MacBook>
2024-09-10 22:24:54 +0200

    Add multi lang support

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
+function setLanguage(lang) {
+    localStorage.setItem('language', lang);
+
+    var json_lang_dictionary = require('lanugages/' + lang + '.json');
+
+    document.getElementById("title").innerHTML = json_lang_dictionary['contrceptions-and-reproduction-health'];
+    document.getElementById("h1_1").innerHTML = json_lang_dictionary['contrceptions-and-reproduction-health'];
+
+}
+
 function toggleMenu() {
     document.body.classList.toggle('openMenu');
 }
-
-/*
-function loadPage(page) {
-    const content = document.getElementById('content');
-
-    if (page === 'eligibility-tool') {
-        content.innerHTML = `<h2>Eligibility tool</h2><p>This is the Eligibility tool page.</p>`;
-    } 
-    
-    else if (page === 'other-condiditons') {
-        content.innerHTML = `<h2>Other conditions</h2><p>This is the Other conditions page.</p>`;
-    } 
-    
-    else if (page === 'additional-info') {
-        content.innerHTML = `<h2>Additional info</h2><p>This is the Additional info page.</p>`;
-    }
-
-    else if (page === 'how-to-use-the-tool') {
-        content.innerHTML = `<h2>How to use the tool</h2><p>This is the How to use the tool page.</p>`;
-    }
-
-    else if (page === 'about') {
-        content.innerHTML = `<h2>About</h2><p>This is the About page.</p>`;
-    }
-
-    else if (page === 'language') {
-        content.innerHTML = `<h2>Language</h2><p>This is the Language page.</p>`;
-    }
-
-    document.body.classList.toggle('openMenu');
-}
-*/
 
 function loadPage(page) {
     const content = document.getElementById('content');
@@ -52,5 +30,7 @@ function loadPage(page) {
         .catch(error => {
             content.innerHTML = `<h2>Error</h2><p>${error.message}</p>`;
         });
+
+    document.body.classList.toggle('openMenu');    
 }
 
