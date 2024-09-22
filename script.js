7d5c2ac69f42396fff89901dfb432ded8bad8e66
Alp <alpguvenir@Alps-MacBook>
2024-09-22 19:25:09 +0200

    Added the age slider

--- script.js
+++ script.js
@@ -66,3 +66,13 @@ function loadPage(page) {
     document.body.classList.toggle('openMenu');    
 }
 
+
+
+
+
+const slider = document.getElementById("slider");
+const output = document.getElementById("slider-value");
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
