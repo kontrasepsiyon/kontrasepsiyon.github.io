d4f88a16b5ef8beb47c2ce7da5d00147d14406e5
Alp <alpguvenir@Alps-MacBook>
2024-09-22 21:24:01 +0200

    Change way to import

--- scripts/eligibility-tool.js
+++ scripts/eligibility-tool.js
@@ -2,5 +2,22 @@ const slider = document.getElementById("styled-slider");
 const output = document.getElementById("slider-value");
 
 slider.oninput = function() {
-  output.innerHTML = slider.value;
+    if (slider.value  == "1") {
+        output.innerHTML = "Menarche to < 18 years"
+    } 
+    else if (slider.value  == "2") {
+        output.innerHTML = "18 - 19"
+    } 
+    else if (slider.value  == "3") {
+        output.innerHTML = "20 - 39"
+    }
+    else if (slider.value  == "4") {
+        output.innerHTML = "40 - 45"
+    }
+    else if (slider.value  == "5") {
+        output.innerHTML = ">= 46"
+    }
+    else {
+        output.innerHTML = "Not selected";
+    }
 };
